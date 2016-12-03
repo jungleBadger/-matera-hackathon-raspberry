9aaf5000daa9a39440f8286b835f78f87f00901a
Daniel Abrão <dcerag@br.ibm.com>
2016-12-03 09:25:33 -0200

    location json

--- server/helpers/deviceLocation.js
+++ server/helpers/deviceLocation.js
@@ -12,7 +12,6 @@
                 return new Promise(function (resolve, reject) {
                     request.get("http://freegeoip.net/json", function (err, body, response) {
                         console.log(err);
-                        console.log(body);
                         resolve(response);
                     });
                 });
